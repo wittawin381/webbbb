563543541e8b85a60802d9813b622e0a01a4e4c5
Boat <werawit_boat@windowslive.com>
2019-11-06 15:30:45 +0700

    test edit home.js

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,9 @@ class App extends Component {
           <h2>Home</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit 
+          LOVE U JUBBUBUB HIN na HEEEE.
+          <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
